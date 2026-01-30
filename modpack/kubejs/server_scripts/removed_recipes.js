781fedc3f4727680dd2bfe88cdc49cf17c9b729b
Ramon <bonesyep@gmail.com>
2026-01-30 20:58:00 -0300

    feat(kubejs): optimized scripts for removing recipes + removed broken guns

--- modpack/kubejs/server_scripts/removed_recipes.js
+++ modpack/kubejs/server_scripts/removed_recipes.js
@@ -1,104 +1,101 @@
-////// REMOVING RECIPES /////////
-//Crc
+////////////////////////////////////
+//////     CONSTANTS       /////////
+////////////////////////////////////
+var GUNS = [
+    "scguns:prima_materia",
+    "scguns:sterilizer",
+    "scguns:defender_pistol",
+    "scguns:greaser_smg",
+    "scguns:m3_carabine",
+    "scguns:stigg",
+    "scguns:minksy",
+    "scguns:raygun",
+    "scguns:rat_king_and_queen",
+    "scguns:locust",
+    "scguns:howler_conversion",
+    "scguns:shard_culler",
+    "scguns:lone_wonder",
+    "scguns:dark_matter",
+    "scguns:gattaler",
+    "scguns:gauss_rifle",
+    "scguns:spitfire",
+    "scguns:flayed_god",
+    "scguns:nervepinch",
+    "scguns:astella",
+    "scguns:big_bore",
+    "scguns:ultra_knight_hawk"
+];
+
+var MEKANISM = [
+    "mekanism:mekasuit_helmet",
+    "mekanism:mekasuit_bodyarmor",
+    "mekanism:mekasuit_pants",
+    "mekanism:mekasuit_boots",
+    "mekanism:meka_tool",
+    "mekanism:atomic_disassembler",
+    "mekanism:basic_universal_cable",
+    "mekanism:advanced_universal_cable",
+    "mekanism:elite_universal_cable",
+    "mekanism:ultimate_universal_cable",
+    "mekanism:digital_miner"
+];
+
+var ANIMAL_SEEDS = [
+    "havenanimalseeds:axolotl_seed",
+    "havenanimalseeds:bee_seed",
+    "havenanimalseeds:cow_seed",
+    "havenanimalseeds:villager_seed",
+    "havenanimalseeds:sniffer_seed",
+    "havenanimalseeds:glow_squid_seed"
+];
+
+var SWORDS = [
+    "minecraft:diamond_sword",
+    "minecraft:netherite_sword",
+    "netherdungeons:piglin_sword",
+    "netherdungeons:wither_skeleton_sword",
+    "ae2:certus_quartz_sword",
+    "ae2:fluix_sword",
+    "ae2:nether_quartz_sword",
+    "clanginghowl:advanced_chainsword",
+    "clanginghowl:extraterrestrial_sword",
+    "manors_bounty:headless_horsemans_cursed_sword",
+    "deeperdarker:resonarium_sword",
+    "deeperdarker:warden_sword",
+    "sgjourney:naquadah_sword"
+];
+
+var CONSTANTS = [
+    GUNS,
+    MEKANISM,
+    ANIMAL_SEEDS,
+    SWORDS
+]
+
+////////////////////////////////////
+//////  REMOVING  RECIPES  /////////
+////////////////////////////////////
 ServerEvents.recipes(event => {
+    CONSTANTS.forEach(item => {
+        event.remove({ output: item });
+    });
+    event.remove({ output: "cyberspace:terminal" });
     event.remove({ output: "crc:portal_opener" });
 })
 
-//Haven Animal Seeds
-ServerEvents.recipes(event => {
-    event.remove({ output: "havenanimalseeds:axolotl_seed" });
-    event.remove({ output: "havenanimalseeds:bee_seed" });
-    event.remove({ output: "havenanimalseeds:cow_seed" });
-    event.remove({ output: "havenanimalseeds:villager_seed" });
-    event.remove({ output: "havenanimalseeds:sniffer_seed" });
-    event.remove({ output: "havenanimalseeds:glow_squid_seed" });
-})
-
-//Mekanism
-ServerEvents.recipes(event => {
-    event.remove({ output: "mekanism:atomic_disassembler" });
-    event.remove({ output: "mekanism:meka_tool" });
-    event.remove({ output: "mekanism:mekasuit_helmet" });
-    event.remove({ output: "mekanism:mekasuit_chestplate" });
-    event.remove({ output: "mekanism:mekasuit_leggings" });
-    event.remove({ output: "mekanism:mekasuit_boots" });
-    event.remove({ output: "mekanism:basic_universal_cable" });
-    event.remove({ output: "mekanism:advanced_universal_cable" });
-    event.remove({ output: "mekanism:elite_universal_cable" });
-    event.remove({ output: "mekanism:ultimate_universal_cable" });
-})
-
-//Swords
-ServerEvents.recipes(event => {
-    event.remove({ output: "minecraft:diamond_sword" });
-    event.remove({ output: "minecraft:netherite_sword" });
-    event.remove({ output: "netherdungeons:piglin_sword" });
-    event.remove({ output: "netherdungeons:wither_skeleton_sword" });
-    event.remove({ output: "ae2:certus_quartz_sword" });
-    event.remove({ output: "ae2:fluix_sword" });
-    event.remove({ output: "ae2:nether_quartz_sword" });
-    event.remove({ output: "clanginghowl:advanced_chainsword" });
-    event.remove({ output: "clanginghowl:extraterrestrial_sword" });
-    event.remove({ output: "manors_bounty:headless_horsemans_cursed_sword" });
-    event.remove({ output: "deeperdarker:resonarium_sword" });
-    event.remove({ output: "deeperdarker:warden_sword" });
-    event.remove({ output: "sgjourney:naquadah_sword" });
-})
-
-//Cyberspace
-ServerEvents.recipes(event => {
-    event.remove({ output: "cyberspace:terminal" });
-})
-
-
+//////////////////////////////////////////////////////
 ///////// HIDING ITEMS FROM RECIPE VIEWERS //////////
-//Crc
+//////////////////////////////////////////////////////
 ServerEvents.tags(`item`, event => {
+    event.add( `c:hidden_from_recipe_viewers`, CONSTANTS );
     event.add( `c:hidden_from_recipe_viewers`, "crc:portal_opener" );
-})
-
-//Mekanism
-ServerEvents.tags(`item`, event => {
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:mekasuit_helmet" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:mekasuit_chestplate" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:mekasuit_leggings" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:mekasuit_boots" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:meka_tool" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:atomic_disassembler" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:basic_universal_cable" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:advanced_universal_cable" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:elite_universal_cable" );
-    event.add( `c:hidden_from_recipe_viewers`, "mekanism:ultimate_universal_cable" );
-})
-
-//Haven Animal Seeds
-ServerEvents.tags(`item`, event => {
-    event.add( `c:hidden_from_recipe_viewers`, "havenanimalseeds:axolotl_seed" );
-    event.add( `c:hidden_from_recipe_viewers`, "havenanimalseeds:bee_seed" );
-    event.add( `c:hidden_from_recipe_viewers`, "havenanimalseeds:cow_seed" );
-    event.add( `c:hidden_from_recipe_viewers`, "havenanimalseeds:villager_seed" );
-    event.add( `c:hidden_from_recipe_viewers`, "havenanimalseeds:sniffer_seed" );
-    event.add( `c:hidden_from_recipe_viewers`, "havenanimalseeds:glow_squid_seed" );
-})
-
-//Swords
-ServerEvents.tags(`item`, event => {
-    event.add( `c:hidden_from_recipe_viewers`, "minecraft:diamond_sword" ); 
-    event.add( `c:hidden_from_recipe_viewers`, "minecraft:netherite_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "netherdungeons:piglin_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "netherdungeons:wither_skeleton_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "ae2:certus_quartz_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "ae2:fluix_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "ae2:nether_quartz_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "clanginghowl:advanced_chainsword" );
-    event.add( `c:hidden_from_recipe_viewers`, "clanginghowl:extraterrestrial_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "manors_bounty:headless_horsemans_cursed_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "deeperdarker:resonarium_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "deeperdarker:warden_sword" );
-    event.add( `c:hidden_from_recipe_viewers`, "sgjourney:naquadah_sword" );
-})
-
-//Cyberspace
-ServerEvents.tags(`item`, event => {
     event.add( `c:hidden_from_recipe_viewers`, "cyberspace:terminal" );
+})
+
+//////////////////////////////////////////////////////
+/////////   REMOVING ITEMS FROM LOOTTABLES  //////////
+//////////////////////////////////////////////////////
+LootJS.modifiers((event) => {
+    event.addLootTypeModifier(LootType.BLOCK).removeLoot(CONSTANTS);
+    event.addLootTypeModifier(LootType.ENTITY).removeLoot('deeperdarker:heart_of_the_deep');
 })

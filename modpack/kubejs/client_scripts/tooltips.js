9e59aca6e15188ab7213e3724060b8106400eb09
Ramon <bonesyep@gmail.com>
2026-01-30 22:27:48 -0300

    mod(tempad): more customization for the tempad

---
+++ modpack/kubejs/client_scripts/tooltips.js
@@ -0,0 +1,8 @@
+ItemEvents.tooltip(event => {
+  event.add('tempad:tempad', [
+    Text.darkPurple('[ Helian Transit & Comms Device ]'),
+    Text.darkPurple('Experimental Helian Technology!!!'),
+    Text.darkPurple('Used for spatial links creation and communication.'),
+    Text.gold('PLANETARY RANGE ONLY')
+  ])
+})

ac9f8a0eb1aeac34a53b3ae408cae99a352bd57c
Ramon <bonesyep@gmail.com>
2026-02-02 21:04:54 -0300

    feat(lightsabers): removed blocking when using lightsaber on off hand too + reduced damage

--- modpack/kubejs/server_scripts/lightsabers.js
+++ modpack/kubejs/server_scripts/lightsabers.js
@@ -1,8 +1,15 @@
 PlayerEvents.tick(event => {
   const player = event.player
-  const item = player.getMainHandItem()
 
-  if (item.id.startsWith("lightsabers:lightsaber_") && player.isUsingItem()) {
+  if (!player.isUsingItem()) return
+
+  const main = player.getMainHandItem()
+  const off = player.getOffHandItem()
+
+  if (
+    main.id.startsWith("lightsabers:lightsaber_") ||
+    off.id.startsWith("lightsabers:lightsaber_")
+  ) {
     player.stopUsingItem()
   }
 })

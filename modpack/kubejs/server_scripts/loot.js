ebaf546a8140dc9c0bccf0f00a9c98eacd4a812d
Ramon <bonesyep@gmail.com>
2026-02-06 20:12:56 -0300

    feat(kubejs): now the smithing templates can actually be found in bumblezone chests (fixed)

--- modpack/kubejs/server_scripts/loot.js
+++ modpack/kubejs/server_scripts/loot.js
@@ -1,16 +1,13 @@
 LootJS.modifiers(event => {
-
   event
-    .addLootTableModifier(/^(the_bumblezone|thebumblezone):chests\/.*/)
+    .addLootTableModifier(/^(the_bumblezone|thebumblezone):structures\/.*/)
     .randomChance(0.35)
     .addLoot("minecraft:netherite_upgrade_smithing_template")
     .apply((ctx) => {
-
       // 25% chance for a second template
       if (Math.random() < 0.25) {
         ctx.addLoot("minecraft:netherite_upgrade_smithing_template")
       }
-
       // 10% chance for a third template
       if (Math.random() < 0.10) {
         ctx.addLoot("minecraft:netherite_upgrade_smithing_template")

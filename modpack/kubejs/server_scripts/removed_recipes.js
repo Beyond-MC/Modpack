8700b10d5f6031346da21de7f9d0bc8d2863eba8
Ramon <bonesyep@gmail.com>
2026-02-08 18:00:45 -0300

    feat(ae2): hiding cable facades cuz theyre too many

--- modpack/kubejs/server_scripts/removed_recipes.js
+++ modpack/kubejs/server_scripts/removed_recipes.js
@@ -127,7 +127,7 @@ ServerEvents.recipes(event => {
 //////////////////////////////////////////////////////
 ServerEvents.tags(`item`, event => {
     event.add( `c:hidden_from_recipe_viewers`, CONSTANTS );
-
+    event.add( `c:hidden_from_recipe_viewers`, /ae2:facade/ )
 })
 
 //////////////////////////////////////////////////////
